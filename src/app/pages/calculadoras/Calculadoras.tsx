8b1ec431f50aa40d4a391060a075c5cf96cc100e
João Pster <joaopsterdev@gmail.com>
2023-02-16 19:50:37 -0300

    feat(Animations): Adicionadas animação na página inicial e de listagem

--- src/app/pages/calculadoras/Calculadoras.tsx
+++ src/app/pages/calculadoras/Calculadoras.tsx
@@ -1,22 +1,53 @@
 import { CaixaDeCalculos } from "../../shared/components";
 import { calculadoras } from "../../../data/calculadoras";
 
+import { motion } from "framer-motion";
+
 import "./Calculadoras.scss";
+
+const list = {
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.3,
+      duration: 0.8,
+    },
+  },
+  initial: {
+    opacity: 0,
+    transition: {
+      when: "afterChildren",
+    },
+  },
+};
+
+const item = {
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  initial: { opacity: 0, y: 100 },
+};
 
 const Calculadoras = () => {
   return (
-    <div className="calcs">
+    <motion.div
+      className="calcs"
+      initial="initial"
+      animate="visible"
+      variants={list}
+    >
       <h2>O que você quer calcular hoje?</h2>
       <div className="caixas">
         {calculadoras.map((calculadora, index) => (
-          <CaixaDeCalculos
-            key={index}
-            titulo={calculadora.name}
-            link={calculadora.path}
-          />
+          <motion.div className="caixaAnimada" key={index} variants={item}>
+            <CaixaDeCalculos
+              key={index}
+              titulo={calculadora.name}
+              link={calculadora.path}
+            />
+          </motion.div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,11 +1,30 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+import { useNavigate } from "react-router-dom";
 
 import "./Home.scss";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/calculadoras");
+  };
+
   return (
-    <div className="image-container">
-      <Link to="/calculadoras">
+    <>
+      <motion.div
+        onDoubleClick={handleClick}
+        className="image-container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { duration: 1.5 } }}
+        exit={{ opacity: 0 }}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        drag="x"
+        dragConstraints={{ left: -100, right: 100 }}
+      >
         <picture>
           <source
             className="logo"
@@ -18,8 +37,15 @@ const Home = () => {
             alt="Browser with large and small images of a coffee cup and plants"
           />
         </picture>
-      </Link>
-    </div>
+      </motion.div>
+      <motion.div
+        className="text-container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { duration: 1.5, delay: 2 } }}
+      >
+        <p>Arraste o icone, e depois clique duas vezes :)</p>
+      </motion.div>
+    </>
   );
 };
 
